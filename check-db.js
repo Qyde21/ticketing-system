e1f0e926eb30a8aab48f8d87ba0cde7717d29d88
Qyde21 <you@example.com>
2026-08-16 14:34:45 +0300

    Remove leftover dev/seed scripts breaking the Vercel build

--- check-db.js
+++
@@ -1,7 +0,0 @@
-﻿import { sql } from "./lib/db.js";
-async function check() {
-  const events = await sql`SELECT * FROM events LIMIT 1`;
-  console.log("Event keys:", Object.keys(events[0] || {}));
-  console.log("Sample event:", events[0]);
-}
-check();
