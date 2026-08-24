c0548b57c561832c37340f48766baa03550f23e3
Qyde21 <kiruiqyde@gmail.com>
2026-08-24 10:51:25 +0300

    CSP: allow full Paystack checkout and Google Maps

--- next.config.ts
+++ next.config.ts
@@ -17,17 +17,21 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co https://*.paystack.co https://maps.googleapis.com https://*.googleapis.com",
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+      // Scripts: Next + Paystack Popup/Inline + Google Maps
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co https://*.paystack.co https://paystack.com https://*.paystack.com https://maps.googleapis.com https://*.googleapis.com",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.paystack.co",
       "img-src 'self' data: blob: https: http:",
-      "font-src 'self' data: https://fonts.gstatic.com",
+      "font-src 'self' data: https://fonts.gstatic.com https://*.paystack.co",
+      // XHR / fetch to Paystack API + everything HTTPS (maps, neon, etc.)
       "connect-src 'self' https: wss:",
-      "frame-src 'self' https://js.paystack.co https://*.paystack.co https://checkout.paystack.com https://accounts.google.com https://maps.google.com https://www.google.com https://maps.googleapis.com https://*.google.com",
-      "child-src 'self' https://maps.google.com https://www.google.com https://*.google.com https://*.paystack.co",
+      // Iframes: Paystack checkout + Google Maps + OAuth
+      "frame-src 'self' https://js.paystack.co https://*.paystack.co https://checkout.paystack.com https://paystack.com https://*.paystack.com https://standard.paystack.co https://accounts.google.com https://maps.google.com https://www.google.com https://maps.googleapis.com https://*.google.com",
+      "child-src 'self' blob: https://js.paystack.co https://*.paystack.co https://checkout.paystack.com https://maps.google.com https://www.google.com https://*.google.com",
       "worker-src 'self' blob:",
       "object-src 'none'",
       "base-uri 'self'",
-      "form-action 'self' https://*.paystack.co https://checkout.paystack.com https://www.google.com",
+      // Form posts into Paystack
+      "form-action 'self' https://*.paystack.co https://checkout.paystack.com https://paystack.com https://*.paystack.com https://www.google.com",
       "frame-ancestors 'self'",
       "upgrade-insecure-requests",
     ].join("; "),
